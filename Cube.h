3c01701dd0f77100302ba90066ee7a51374bf3d6
= <kuba.kaczmarski312@gmail.com>
2022-06-25 15:46:45 +0200

    Rendering map in real time

--- Cube.h
+++ Cube.h
@@ -6,7 +6,7 @@
 /*   By: jkaczmar <jkaczmar@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/23 15:19:52 by jkaczmar          #+#    #+#             */
-/*   Updated: 2022/06/25 14:19:23 by jkaczmar         ###   ########.fr       */
+/*   Updated: 2022/06/25 15:35:19 by jkaczmar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,16 +18,15 @@
 #include <stdlib.h>
 
 
-#define WIDTH 1280
-#define HEIGHT 768
+
 #define PI 3.14159265359
 
-typedef struct s_main_thingy
+typedef struct mlx_and_image
 {
-    mlx_t *mlx;
-    mlx_image_t *image_thingy;
-    // mlx_image_t *wall_thingy;
-}   t_main_thingy;
-
+  mlx_t *mlx;
+  //Index 0 reserved for the player
+  mlx_image_t **img_arr;
+    
+} mai_t;
 
 #endif
--- main.c
+++ main.c
@@ -6,141 +6,133 @@
 /*   By: jkaczmar <jkaczmar@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/22 13:14:43 by jkaczmar          #+#    #+#             */
-/*   Updated: 2022/06/25 14:26:47 by jkaczmar         ###   ########.fr       */
+/*   Updated: 2022/06/25 15:46:06 by jkaczmar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdio.h>
 #include "MLX42/include/MLX42/MLX42.h"
-
-int worldMap[24][24]=
-{
-  {1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1},
-  {1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
-  {1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
-  {1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
-  {1,0,0,0,0,0,2,2,2,2,2,0,0,0,0,3,0,3,0,3,0,0,0,1},
-  {1,0,0,0,0,0,2,0,0,0,2,0,0,0,0,0,0,0,0,0,0,0,0,1},
-  {1,0,0,0,0,0,2,0,0,0,2,0,0,0,0,3,0,0,0,3,0,0,0,1},
-  {1,0,0,0,0,0,2,0,0,0,2,0,0,0,0,0,0,0,0,0,0,0,0,1},
-  {1,0,0,0,0,0,2,2,0,2,2,0,0,0,0,3,0,3,0,3,0,0,0,1},
-  {1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
-  {1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
-  {1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
-  {1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
-  {1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
-  {1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
-  {1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
-  {1,4,4,4,4,4,4,4,4,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
-  {1,4,0,4,0,0,0,0,4,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
-  {1,4,0,0,0,0,5,0,4,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
-  {1,4,0,4,0,0,0,0,4,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
-  {1,4,0,4,4,4,4,4,4,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
-  {1,4,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
-  {1,4,4,4,4,4,4,4,4,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
-  {1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1}
-};
-
-
-// void	hook(void *param)
-// {
-// 	mlx_t	*mlx;
-
-// 	mlx = param;
-// 	if (mlx_is_key_down(param, MLX_KEY_ESCAPE))
-// 		mlx_close_window(param);
-// 	if (mlx_is_key_down(param, MLX_KEY_UP))
-// 		img->instances[0].y -= 5;
-// 	if (mlx_is_key_down(param, MLX_KEY_DOWN))
-// 		img->instances[0].y += 5;
-// 	if (mlx_is_key_down(param, MLX_KEY_LEFT))
-// 		img->instances[0].x -= 5;
-// 	if (mlx_is_key_down(param, MLX_KEY_RIGHT))
-// 		img->instances[0].x += 5;
-// }
-
-
-#include <string.h>
-#include <stdlib.h>
+#define WIDTH 1280
+#define HEIGHT 768
 #define IMG_SIDE 80
 #define ROWS 12
 #define COLS 6
+#define PLAYER_SIZE 20
+#include <string.h>
+// #include "Cube.h"/
+#include <stdio.h>
+#include <math.h>
+#include <stdlib.h>
 #define WINDOW_WIDTH ((ROWS * IMG_SIDE) + ROWS - 1)
 #define WINDOW_HEIGHT ((COLS * IMG_SIDE) + COLS - 1)
-
 typedef struct mlx_and_image
 {
   mlx_t *mlx;
-  mlx_image_t *img;
-  mlx_image_t *wall;
+  //Index 0 reserved for the player
+  mlx_image_t **img_arr;
     
 } mai_t;
-
-void draw_grid(mai_t *mlx)
+int worldMap[9][9]=
 {
-  mlx->img = mlx_new_image(mlx->mlx, IMG_SIDE, IMG_SIDE);
-  memset(mlx->img->pixels, 255, IMG_SIDE * IMG_SIDE * sizeof(int));
-  mlx->wall = mlx_new_image(mlx->mlx, IMG_SIDE, IMG_SIDE);
+  {1,1,1,1,1,1,1,1,1},
+  {1,0,0,0,1,0,0,0,1},
+  {1,0,0,0,1,0,0,0,1},
+  {1,0,0,0,0,0,0,0,1},
+  {1,0,0,0,0,0,0,0,1},
+  {1,0,0,0,0,0,0,0,1},
+  {1,0,0,0,0,0,0,0,1},
+  {1,0,0,0,1,0,0,0,1},
+  {1,1,1,1,1,1,1,1,1}
+};
+
+
+
+
+void load_textures(mai_t *mlx)
+{
+  if(mlx){}
+  //Empty space
+  mlx->img_arr[1] = mlx_new_image(mlx->mlx, IMG_SIDE, IMG_SIDE);
   int i = 0;
   int j = 0;
   while(i < IMG_SIDE)
-	{
-		j = 0;
-		while(j < IMG_SIDE)
-		{
-			mlx_put_pixel(mlx->wall, i, j, 0x000FFFF);
-			j++;
-		}
-
-		i++;
-	}
-  for(int i = 0; i < mlx->mlx->height; i += IMG_SIDE + 1)
-    for(int j = 0; j < mlx->mlx->width; j += IMG_SIDE + 1)
-	{
-		if(j == 0 || j + IMG_SIDE + 1 > mlx->mlx->width || i == 0 || i + IMG_SIDE + 1 > mlx->mlx->height)
-		{
-			mlx_image_to_window(mlx->mlx, mlx->wall, j, i);
-		}else{
-			mlx_image_to_window(mlx->mlx, mlx->img, j, i);
-		}
-	}
+  {
+    j = 0;
+    while(j < IMG_SIDE)
+    {
+      mlx_put_pixel(mlx->img_arr[1], i, j, 0x0000FF);
+      j++;
+    }
+    i++;
+  }
+  //Wall
+  mlx->img_arr[2] = mlx_new_image(mlx->mlx, IMG_SIDE, IMG_SIDE);
+  //Load_colors for now
 }
 
-void draw_player(mai_t *yes)
+
+void draw_grid(mai_t *mlx)
 {
-  yes->img = mlx_new_image(yes->mlx, IMG_SIDE, IMG_SIDE);
-  memset(yes->img->pixels, 99, IMG_SIDE * IMG_SIDE * sizeof(int));  
-  mlx_image_to_window(yes->mlx, yes->img, 0, 0);
+  load_textures(mlx);
+  //Loop through the map
+  int i = 0;
+  int j = 0;
+  int y = 0;
+  int x = 0;;
+  while(i < 9)
+  {
+    j = 0;
+    y = 0;
+    while(j < 9)
+    {
+      perror("Zium");
+        if(worldMap[i][j] == 1)
+          mlx_image_to_window(mlx->mlx ,mlx->img_arr[1],x, y);
+        y += IMG_SIDE;
+        j++;
+    }
+    x += IMG_SIDE;
+    i++;
+  }
+}
+
+void draw_player(mai_t *mlx_info)
+{
+  mlx_info->img_arr[0] = mlx_new_image(mlx_info->mlx, PLAYER_SIZE, PLAYER_SIZE);
+  memset(mlx_info->img_arr[0]->pixels, 255, PLAYER_SIZE * PLAYER_SIZE * sizeof(int));  
+  mlx_image_to_window(mlx_info->mlx, mlx_info->img_arr[0], 0, 0);
 }
 
 void movement_hook(void *x)
 {
-  mai_t *yes;
+  mai_t *mlx_info;
   
-  yes = x;
-  if (mlx_is_key_down(yes->mlx, MLX_KEY_ESCAPE))
-    mlx_close_window(yes->mlx);
-  if (mlx_is_key_down(yes->mlx, MLX_KEY_UP))
-		yes->img->instances->y -= 5;
-  if (mlx_is_key_down(yes->mlx, MLX_KEY_DOWN))
-		yes->img->instances->y += 5;
-  if (mlx_is_key_down(yes->mlx, MLX_KEY_LEFT))
-		yes->img->instances->x -= 5;
-  if (mlx_is_key_down(yes->mlx, MLX_KEY_RIGHT))
-		yes->img->instances->x += 5;
+  mlx_info = x;
+  if (mlx_is_key_down(mlx_info->mlx, MLX_KEY_ESCAPE))
+    mlx_close_window(mlx_info->mlx);
+  if (mlx_is_key_down(mlx_info->mlx, MLX_KEY_UP))
+		mlx_info->img_arr[0]->instances->y -= 5;
+  if (mlx_is_key_down(mlx_info->mlx, MLX_KEY_DOWN))
+		mlx_info->img_arr[0]->instances->y += 5;
+  if (mlx_is_key_down(mlx_info->mlx, MLX_KEY_LEFT))
+		mlx_info->img_arr[0]->instances->x -= 5;
+  if (mlx_is_key_down(mlx_info->mlx, MLX_KEY_RIGHT))
+		mlx_info->img_arr[0]->instances->x += 5;
 }
 
-
-int main(void)
+int	main(void)
 {
-  mai_t yes;
-  
-	yes.mlx = mlx_init(WINDOW_WIDTH, WINDOW_HEIGHT, "Kurwiszon", true);
-  if (!yes.mlx)
+  //Before rendering get the map dimensions
+  int x = 9;
+  int y = 9;
+  mai_t mlx_info;
+	mlx_info.mlx = mlx_init(x * IMG_SIDE, y * IMG_SIDE, "Kurwiszon", true);
+  if (!mlx_info.mlx)
 		exit(-99);
-  draw_grid(&yes);
-  draw_player(&yes);
-  mlx_loop_hook(yes.mlx, &movement_hook, &yes);
-	mlx_loop(yes.mlx);
+  mlx_info.img_arr = calloc(5 ,sizeof(mlx_image_t));
+  draw_grid(&mlx_info);
+  draw_player(&mlx_info);
+  mlx_loop_hook(mlx_info.mlx, &movement_hook, &mlx_info);
+	mlx_loop(mlx_info.mlx);
 	return (0);
 }
