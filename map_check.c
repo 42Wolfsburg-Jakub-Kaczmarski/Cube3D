b09c026d293d06c1be36e59eef16b27605e82bfd
= <kuba.kaczmarski312@gmail.com>
2022-06-27 23:43:56 +0200

    Charac check

--- map_check.c
+++ map_check.c
@@ -6,16 +6,39 @@
 /*   By: jkaczmar <jkaczmar@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/25 17:46:08 by jkaczmar          #+#    #+#             */
-/*   Updated: 2022/06/27 21:46:16 by jkaczmar         ###   ########.fr       */
+/*   Updated: 2022/06/27 23:43:44 by jkaczmar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Cube.h"
 
+int	check_if_right_characs(char **arr)
+{
+	int	i;
+	int	j;
+
+	i = 0;
+	j = 0;
+	while (arr[i][j])
+	{
+		j = 0;
+		while (arr[i][j])
+		{
+			if (arr[i][j] != '1' && arr[i][j] != '0' && arr[i][j] != 'P' && arr[i][j] != '\n' )
+			{
+				return (-1);
+			}
+			j++;
+		}
+		i++;
+	}
+	return (1);
+}
+
 int	check_map(char **argv,  mai_t *mlx_info)
 {
 	int fd = open(argv[1], O_RDONLY);
-	if(fd < 0)
+	if(fd < 0 )
 	{
 		printf("File doesn't exists or we don't have right to open it\n");
 		return 0;
@@ -40,7 +63,8 @@ int	check_map(char **argv,  mai_t *mlx_info)
 		mlx_info->map[counter++] = line;
 		line = get_next_line(fd);
 	}
-
+ 	if(check_if_right_characs(mlx_info->map) == -1)
+	 	return 0;
 	// counter--;
 	// while(counter != 0)
 	// {
