1792ec10b0e82f2cd0295ea436345d862a620ca1
= <kuba.kaczmarski312@gmail.com>
2022-07-07 14:56:04 +0200

    Splitting files make code readeble plz

---
+++ Movement/arrows.c
@@ -0,0 +1,37 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   arrows.c                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jkaczmar <jkaczmar@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/07/07 14:54:46 by jkaczmar          #+#    #+#             */
+/*   Updated: 2022/07/07 14:55:01 by jkaczmar         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../Cube.h"
+
+void	key_right_arr(t_mlx_info *mlx_info)
+{
+	double oldDirX = mlx_info->unique_prop.dirX;
+	mlx_info->unique_prop.dirX = mlx_info->unique_prop.dirX * cos(-mlx_info->unique_prop.rotSpeed) - mlx_info->unique_prop.dirY * sin(-mlx_info->unique_prop.rotSpeed);
+	mlx_info->unique_prop.dirY = oldDirX * sin(-mlx_info->unique_prop.rotSpeed)  + mlx_info->unique_prop.dirY * cos(-mlx_info->unique_prop.rotSpeed);
+	double oldplaneX = mlx_info->unique_prop.planeX;
+	mlx_info->unique_prop.planeX = mlx_info->unique_prop.planeX * cos(-mlx_info->unique_prop.rotSpeed) - mlx_info->unique_prop.planeY * sin(-mlx_info->unique_prop.rotSpeed);
+	mlx_info->unique_prop.planeY = oldplaneX * sin(-mlx_info->unique_prop.rotSpeed) + mlx_info->unique_prop.planeY * cos(-mlx_info->unique_prop.rotSpeed);
+	mlx_clear_window(mlx_info->mlx, mlx_info->main_win);
+	render(mlx_info);
+}
+
+void	key_left_arr(t_mlx_info *mlx_info)
+{
+	double oldDirX = mlx_info->unique_prop.dirX;
+	mlx_info->unique_prop.dirX = mlx_info->unique_prop.dirX * cos(mlx_info->unique_prop.rotSpeed) - mlx_info->unique_prop.dirY * sin(mlx_info->unique_prop.rotSpeed);
+	mlx_info->unique_prop.dirY = oldDirX * sin(mlx_info->unique_prop.rotSpeed)  + mlx_info->unique_prop.dirY * cos(mlx_info->unique_prop.rotSpeed);
+	double oldplaneX = mlx_info->unique_prop.planeX;
+	mlx_info->unique_prop.planeX = mlx_info->unique_prop.planeX * cos(mlx_info->unique_prop.rotSpeed) - mlx_info->unique_prop.planeY * sin(mlx_info->unique_prop.rotSpeed);
+	mlx_info->unique_prop.planeY = oldplaneX * sin(mlx_info->unique_prop.rotSpeed) + mlx_info->unique_prop.planeY * cos(mlx_info->unique_prop.rotSpeed);
+	mlx_clear_window(mlx_info->mlx, mlx_info->main_win);
+	render(mlx_info);
+}
---
+++ Movement/key_hook.c
@@ -0,0 +1,37 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   key_hook.c                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jkaczmar <jkaczmar@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/07/07 14:53:02 by jkaczmar          #+#    #+#             */
+/*   Updated: 2022/07/07 14:55:48 by jkaczmar         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../Cube.h"
+
+
+int	key_hook(int keycode,void *mlx)
+{
+	t_mlx_info *mlx_info = (t_mlx_info *) mlx;
+	if(keycode == 13)
+		key_W_hook(mlx_info);
+	if (keycode == 1)
+		key_S(mlx_info);
+	if(keycode == 0)
+		key_A(mlx_info);
+	if(keycode == 2)
+		key_D(mlx_info);
+	if(keycode == 124)
+		key_right_arr(mlx_info);
+	if(keycode == 123)
+		key_left_arr(mlx_info);
+	if(keycode == 53)
+	{
+		mlx_destroy_window(mlx_info->mlx, mlx_info->main_win);
+		exit(1);
+	}
+	return 0;
+}
