7184533f877084f92b3536cf6f7bf90c6868508a
kmilchev <kmilchev@student.42wolfsburg.de>
2022-07-15 15:54:05 +0200

    movement should work

--- Movement/arrows.c
+++ Movement/arrows.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   arrows.c                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: jkaczmar <jkaczmar@student.42.fr>          +#+  +:+       +#+        */
+/*   By: kmilchev <kmilchev@student.42wolfsburg.de> +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/07 14:54:46 by jkaczmar          #+#    #+#             */
-/*   Updated: 2022/07/14 19:01:22 by jkaczmar         ###   ########.fr       */
+/*   Updated: 2022/07/15 14:57:24 by kmilchev         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,8 +20,8 @@ void	key_right_arr(t_mlx_info *mlx_info)
 	double oldplaneX = mlx_info->unique_prop.planeX;
 	mlx_info->unique_prop.planeX = mlx_info->unique_prop.planeX * cos(-mlx_info->unique_prop.rotSpeed) - mlx_info->unique_prop.planeY * sin(-mlx_info->unique_prop.rotSpeed);
 	mlx_info->unique_prop.planeY = oldplaneX * sin(-mlx_info->unique_prop.rotSpeed) + mlx_info->unique_prop.planeY * cos(-mlx_info->unique_prop.rotSpeed);
-	mlx_clear_window(mlx_info->mlx, mlx_info->main_win);
-	render(mlx_info);
+	// mlx_clear_window(mlx_info->mlx, mlx_info->main_win);
+	// render(mlx_info);
 }
 
 void	key_left_arr(t_mlx_info *mlx_info)
@@ -32,6 +32,6 @@ void	key_left_arr(t_mlx_info *mlx_info)
 	double oldplaneX = mlx_info->unique_prop.planeX;
 	mlx_info->unique_prop.planeX = mlx_info->unique_prop.planeX * cos(mlx_info->unique_prop.rotSpeed) - mlx_info->unique_prop.planeY * sin(mlx_info->unique_prop.rotSpeed);
 	mlx_info->unique_prop.planeY = oldplaneX * sin(mlx_info->unique_prop.rotSpeed) + mlx_info->unique_prop.planeY * cos(mlx_info->unique_prop.rotSpeed);
-	mlx_clear_window(mlx_info->mlx, mlx_info->main_win);
-	render(mlx_info);
+	// mlx_clear_window(mlx_info->mlx, mlx_info->main_win);
+	// render(mlx_info);
 }
--- Movement/keys.c
+++ Movement/keys.c
@@ -6,7 +6,7 @@
 /*   By: kmilchev <kmilchev@student.42wolfsburg.de> +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/07 14:43:05 by jkaczmar          #+#    #+#             */
-/*   Updated: 2022/07/15 14:17:01 by kmilchev         ###   ########.fr       */
+/*   Updated: 2022/07/15 15:27:41 by kmilchev         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,25 +19,36 @@ void key_W_hook(t_mlx_info *mlx_info)
 
 	printf("Up________________________\n");
 	prop = &(mlx_info->unique_prop);
-	int x = (int)prop->posX + (int)prop->dirX * (int)prop->move_speed;
-	int y = (int)prop->posY - (int)prop->dirY * (int)prop->move_speed;
-	// printf("Current x: %d\n", (int)prop->posX);
-	// printf("Current y: %d\n", (int)prop->posY );
-	// printf("Speed: %d\n", (int)prop->move_speed);
-	// printf("Additional x: %d\n", (int)prop->dirX * (int)prop->move_speed);
-	// printf("Substract y: %d\n", (int)prop->dirY * (int)prop->move_speed );
-	// 	printf("Projected x: %d\n", x);
-	// 	printf("Projected y: %d\n", y);
-	// printf("Status %d\n", (x > 1 && x < mapHeight - 1));
-	// printf("here\n");
-	if( 
-		worldMap[x][y] != 1)
+	int x = (int)prop->posX + (int)(prop->dirX * prop->move_speed);
+	int y = (int)prop->posY - (int)(prop->dirY * prop->move_speed);
+	double temp_X = prop->posX + prop->dirX * prop->move_speed;
+	double temp_Y = prop->posY + prop->dirY * prop->move_speed;
+	printf("Current x: %d\n", (int)prop->posX);
+	printf("Current y: %d\n", (int)prop->posY );
+	printf("Speed: %d\n", (int)prop->move_speed);
+	printf("Additional x: %d\n", (int)prop->dirX * (int)prop->move_speed);
+	printf("Substract y: %d\n", (int)prop->dirY * (int)prop->move_speed );
+		printf("Projected x: %d\n", x);
+		printf("Projected y: %d\n", y);
+	printf("Status %d\n", (x > 1 && x < mapHeight - 1));
+	printf("here\n");
+	if( temp_X > 1 && temp_X < mapWidth - 1 && temp_Y > 1 && temp_Y < mapHeight - 1)
 	{
+		printf("Passed first check\n");
 		printf("Square value: %d\n", worldMap[x][y]);
-		printf("Current x: %d\n", x);
-		printf("Current y: %d\n", y);
-		prop->posX += prop->dirX * prop->move_speed;
-		prop->posY += prop->dirY * prop->move_speed;
+		if (worldMap[x][y] != 2)
+		{
+			printf("Square value: %d\n", worldMap[x][y]);
+			printf("Current x in: %d\n", x);
+			printf("Current y in: %d\n", y);
+			printf("Position X: %f\n", prop->posX + prop->dirX * prop->move_speed);
+			prop->posX += prop->dirX * prop->move_speed;
+			prop->posY += prop->dirY * prop->move_speed;
+		}
+		else
+		{
+			printf("why am I even here????\n");
+		}
 	}
 	// if(( x2 > 1 && x2 < mapHeight - 1) && (y2 > 1 && y2 < mapWidth) &&
 	// 	worldMap[(int)prop->posX][(int)prop->posY - (int)prop->dirY * (int)prop->move_speed ] != 1)
@@ -52,15 +63,18 @@ void	key_S(t_mlx_info *mlx_info)
 	printf("Down________________________\n");
 	prop = &(mlx_info->unique_prop);
 	int x = (int)prop->posX - (int)prop->dirX * (int) prop->move_speed;
-	int y = (int)prop->posY;
+	int y = (int)prop->posY - (int)prop->dirY * (int)prop->move_speed ;
+	double temp_X = prop->posX - prop->dirX * prop->move_speed;
+	double temp_Y = prop->posY - prop->dirY * prop->move_speed;
 	printf("x: %d\n", x);
 	printf("y: %d\n", y);
-	if((x > 0 && x < mapHeight -2 ) && (y > 1 && y < mapWidth - 2) &&
+	if( temp_X > 1 && temp_X < mapWidth - 1 && temp_Y > 1 && temp_Y < mapHeight - 1 &&
 		worldMap[x][y] != 1)
 	{
 		printf("Square value: %d\n", worldMap[x][y]);
-		printf("Current x: %d\n", x);
-		printf("Current y: %d\n", y);
+		printf("Current x in: %d\n", x);
+		printf("Current y in: %d\n", y);
+		printf("Position X: %f\n", prop->posX - prop->dirX * prop->move_speed);
 		prop->posX -= prop->dirX * prop->move_speed;
 		prop->posY -= prop->dirY * prop->move_speed;
 	}
@@ -74,11 +88,22 @@ void	key_A(t_mlx_info *mlx_info)
 	prop = &(mlx_info->unique_prop);
 	int x = (int)prop->posX - (int)prop->planeX * (int)prop->move_speed;
 	int y = (int)prop->posY - (int)prop->planeY * (int)prop->move_speed;
-	if(worldMap[x][y] != 1)
+	double temp_X = prop->posX - prop->planeX * prop->move_speed;
+	double temp_Y = prop->posY - prop->planeY * prop->move_speed;
+	printf("Current x: %d\n", (int)prop->posX);
+	printf("Current y: %d\n", (int)prop->posY );
+	printf("Speed: %d\n", (int)prop->move_speed);
+	printf("Change of x: %f\n", prop->planeX * (int)prop->move_speed);
+	printf("Change of y: %f\n", prop->planeY * (int)prop->move_speed);
+		printf("Projected x: %d\n", x);
+		printf("Projected y: %d\n", y);
+	printf("Status %d\n", (x > 1 && x < mapHeight - 1));
+	if(temp_X > 1 && temp_X < mapWidth - 1 && temp_Y > 1 && temp_Y < mapHeight - 1 &&
+		worldMap[x][y] != 1)
 	{
 		printf("Square value: %d\n", worldMap[x][y]);
-		printf("Current x: %d\n", x);
-		printf("Current y: %d\n", y);
+		printf("Current x in: %d\n", x);
+		printf("Current y in: %d\n", y);
 		prop->posX -= prop->planeX * prop->move_speed;
 		prop->posY -= prop->planeY * prop->move_speed;
 	}
@@ -89,14 +114,28 @@ void	key_D(t_mlx_info *mlx_info)
 	t_uniq_prop *prop;
 
 	printf("Right________________________\n");
+	
 	prop = &(mlx_info->unique_prop);
-	int x = (int)prop->posX - (int)prop->planeX * (int)prop->move_speed;
-	int y = (int)prop->posY - (int)prop->planeY * (int)prop->move_speed;
-	if(worldMap[x][y] != 1)
+	int x = prop->posX - (int)prop->planeX * (int)prop->move_speed;
+	int y = prop->posY - (int)prop->planeY * (int)prop->move_speed;
+	double temp_X = prop->posX + prop->planeX * prop->move_speed;
+	double temp_Y = prop->posY + prop->planeY * prop->move_speed;
+	printf("Current x: %d\n", (int)prop->posX);
+	printf("Current y: %d\n", (int)prop->posY );
+	printf("Speed: %d\n", (int)prop->move_speed);
+	printf("Change of x: %f\n", prop->planeX * (int)prop->move_speed);
+	printf("Change of y: %f\n", prop->planeY * (int)prop->move_speed);
+		printf("Projected x: %d\n", x);
+		printf("Projected y: %d\n", y);
+	printf("Status %d\n", (x > 1 && x < mapHeight - 1));
+
+	
+	if(temp_X > 1 && temp_X < mapWidth - 1 && temp_Y > 1 && temp_Y < mapHeight - 1 &&
+		worldMap[x][y] != 1)
 	{
 		printf("Square value: %d\n", worldMap[x][y]);
-		printf("Current x: %d\n", x);
-		printf("Current y: %d\n", y);
+		printf("Current x in: %d\n", x);
+		printf("Current y in: %d\n", y);
 		prop->posX += prop->planeX * prop->move_speed;
 		prop->posY += prop->planeY * prop->move_speed;
 
