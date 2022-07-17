e7c21ebff1348096211ff1b52b98819fcd861bea
kmilchev <kmilchev@student.42wolfsburg.de>
2022-07-17 12:35:07 +0200

    norminneted arrows

--- Movement/arrows.c
+++ Movement/arrows.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   arrows.c                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: jkaczmar <jkaczmar@student.42.fr>          +#+  +:+       +#+        */
+/*   By: kmilchev <kmilchev@student.42wolfsburg.de> +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/07 14:54:46 by jkaczmar          #+#    #+#             */
-/*   Updated: 2022/07/16 21:52:34 by jkaczmar         ###   ########.fr       */
+/*   Updated: 2022/07/17 12:34:42 by kmilchev         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,40 +14,38 @@
 
 void	key_right_arr(t_mlx_info *mlx_info)
 {
-	double	oldDirX;
-	double	oldplaneX;
+	double		old_dir_x;
+	double		old_plane_x;
+	t_uniq_prop	*unique_prop;
 
-	oldDirX = mlx_info->unique_prop.dirX;
-	mlx_info->unique_prop.dirX =
-		mlx_info->unique_prop.dirX * cos(-mlx_info->unique_prop.rotSpeed) -
-		mlx_info->unique_prop.dirY * sin(-mlx_info->unique_prop.rotSpeed);
-	mlx_info->unique_prop.dirY = oldDirX * sin(-mlx_info->unique_prop.rotSpeed) +
-		mlx_info->unique_prop.dirY * cos(-mlx_info->unique_prop.rotSpeed);
-	oldplaneX = mlx_info->unique_prop.planeX;
-	mlx_info->unique_prop.planeX =
-		mlx_info->unique_prop.planeX * cos(-mlx_info->unique_prop.rotSpeed) -
-		mlx_info->unique_prop.planeY * sin(-mlx_info->unique_prop.rotSpeed);
-	mlx_info->unique_prop.planeY =
-		oldplaneX * sin(-mlx_info->unique_prop.rotSpeed) +
-		mlx_info->unique_prop.planeY * cos(-mlx_info->unique_prop.rotSpeed);
+	unique_prop = &mlx_info->unique_prop;
+	old_dir_x = unique_prop->dirX;
+	unique_prop->dirX = unique_prop->dirX * cos(-unique_prop->rotSpeed)
+		- unique_prop->dirY * sin(-unique_prop->rotSpeed);
+	unique_prop->dirY = old_dir_x * sin(-unique_prop->rotSpeed)
+		+ unique_prop->dirY * cos(-unique_prop->rotSpeed);
+	old_plane_x = unique_prop->planeX;
+	unique_prop->planeX = unique_prop->planeX * cos(-unique_prop->rotSpeed)
+		- unique_prop->planeY * sin(-unique_prop->rotSpeed);
+	unique_prop->planeY = old_plane_x * sin(-unique_prop->rotSpeed)
+		+ unique_prop->planeY * cos(-unique_prop->rotSpeed);
 }
 
 void	key_left_arr(t_mlx_info *mlx_info)
 {
-	double	oldDirX;
-	double	oldplaneX;
+	double		old_dir_x;
+	double		old_plane_x;
+	t_uniq_prop	*unique_prop;
 
-	oldDirX = mlx_info->unique_prop.dirX;
-	mlx_info->unique_prop.dirX =
-		mlx_info->unique_prop.dirX * cos(mlx_info->unique_prop.rotSpeed) -
-		mlx_info->unique_prop.dirY * sin(mlx_info->unique_prop.rotSpeed);
-	mlx_info->unique_prop.dirY = oldDirX * sin(mlx_info->unique_prop.rotSpeed) +
-		mlx_info->unique_prop.dirY * cos(mlx_info->unique_prop.rotSpeed);
-	oldplaneX = mlx_info->unique_prop.planeX;
-	mlx_info->unique_prop.planeX =
-		mlx_info->unique_prop.planeX * cos(mlx_info->unique_prop.rotSpeed) -
-		mlx_info->unique_prop.planeY * sin(mlx_info->unique_prop.rotSpeed);
-	mlx_info->unique_prop.planeY =
-		oldplaneX * sin(mlx_info->unique_prop.rotSpeed) +
-		mlx_info->unique_prop.planeY * cos(mlx_info->unique_prop.rotSpeed);
+	unique_prop = &mlx_info->unique_prop;
+	old_dir_x = unique_prop->dirX;
+	unique_prop->dirX = unique_prop->dirX * cos(unique_prop->rotSpeed)
+		- unique_prop->dirY * sin(unique_prop->rotSpeed);
+	unique_prop->dirY = old_dir_x * sin(unique_prop->rotSpeed)
+		+ unique_prop->dirY * cos(unique_prop->rotSpeed);
+	old_plane_x = unique_prop->planeX;
+	unique_prop->planeX = unique_prop->planeX * cos(unique_prop->rotSpeed)
+		- unique_prop->planeY * sin(unique_prop->rotSpeed);
+	unique_prop->planeY = old_plane_x * sin(unique_prop->rotSpeed)
+		+ unique_prop->planeY * cos(unique_prop->rotSpeed);
 }
