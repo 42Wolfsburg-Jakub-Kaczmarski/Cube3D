ccf9fa4e37ae1acb26178402e29879fde472f610
kmilchev <kmilchev@student.42wolfsburg.de>
2022-08-02 03:02:59 +0200

    fixed movement smoothnes on arrow keys

--- src/Movement/key_hook.c
+++ src/Movement/key_hook.c
@@ -6,7 +6,7 @@
 /*   By: kmilchev <kmilchev@student.42wolfsburg.de> +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/07 14:53:02 by jkaczmar          #+#    #+#             */
-/*   Updated: 2022/08/01 14:53:24 by kmilchev         ###   ########.fr       */
+/*   Updated: 2022/08/02 02:38:17 by kmilchev         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -38,9 +38,9 @@ int	key_is_pressed(int keycode, void *mlx)
 	if (keycode == 49)
 		key_space(mlx_info);
 	if (keycode == 124)
-		key_right_arr(mlx_info, 10);
+		mlx_info->keys.right_arrow = true;
 	if (keycode == 123)
-		key_left_arr(mlx_info, 10);
+		mlx_info->keys.left_arrow = true;
 	if (keycode == 53)
 	{
 		mlx_destroy_window(mlx_info->mlx, mlx_info->main_win);
@@ -63,6 +63,10 @@ int	key_is_released(int keycode, void *mlx)
 		mlx_info->keys.a = false;
 	if (keycode == 2)
 		mlx_info->keys.d = false;
+	if (keycode == 124)
+		mlx_info->keys.right_arrow = false;
+	if (keycode == 123)
+		mlx_info->keys.left_arrow = false;
 	return (0);
 }
 
@@ -76,5 +80,9 @@ int	key_movement(t_mlx_info *mlx_info)
 		key_a(mlx_info);
 	if (mlx_info->keys.d == true)
 		key_d(mlx_info);
+	if (mlx_info->keys.right_arrow == true)
+		key_right_arr(mlx_info, 1);
+	if (mlx_info->keys.left_arrow == true)
+		key_left_arr(mlx_info, 1);
 	return (0);
 }
