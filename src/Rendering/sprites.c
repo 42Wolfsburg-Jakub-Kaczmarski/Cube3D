5c81e3fd4eb1b1a9e9e4eac7b0201494a865c8af
kmilchev <kmilchev@student.42wolfsburg.de>
2022-08-03 15:31:49 +0200

    norminetted

--- src/Rendering/sprites.c
+++ src/Rendering/sprites.c
@@ -3,46 +3,48 @@
 /*                                                        :::      ::::::::   */
 /*   sprites.c                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: jkaczmar <jkaczmar@student.42.fr>          +#+  +:+       +#+        */
+/*   By: kmilchev <kmilchev@student.42wolfsburg.de> +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/19 21:53:25 by jkaczmar          #+#    #+#             */
-/*   Updated: 2022/07/22 21:16:03 by jkaczmar         ###   ########.fr       */
+/*   Updated: 2022/08/03 15:31:23 by kmilchev         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../include/Cube.h"
 
-void	load_sprites(t_mlx_info *mlx_info)
+void	load_sprites(t_mlx_info *m)
 {
-		int i = 4;
-		char **str = ft_calloc(4, sizeof(char *));
-		str[0] = ft_strdup("./assets/cat_fighter.xpm");
-		str[1] = ft_strdup("./assets/cuttlefish.xpm");
-		str[2] = ft_strdup("./assets/fish.xpm");
-		int j = 0;
-		while(i < 7)
-		{
-				mlx_info->mlx_imgs[i] = mlx_xpm_file_to_image(mlx_info->mlx,
-				str[j],
-				&mlx_info->texture_data[i].width,
-				&mlx_info->texture_data[i].height);
-		mlx_info->im[i].img_data = mlx_get_data_addr(mlx_info->mlx_imgs[i],
-				&mlx_info->im[i].img_bp,
-				&mlx_info->im[i].img_sl,
-				&mlx_info->im[i].img_e);
-		mlx_info->texture_data[i].arr_color = create_color_arr(&mlx_info->im[i],
-				mlx_info->texture_data[i].height,
-				mlx_info->texture_data[i].width);
-		mlx_info->texture_data[i].img_h = mlx_info->mlx_imgs[i];
-			j++;
-			i++;
-		}
-		free_2d_array(str);
+	int		i;
+	char	**str;
+
+	str = ft_calloc(4, sizeof(char *));
+	str[0] = ft_strdup("./assets/cat_fighter.xpm");
+	str[1] = ft_strdup("./assets/cuttlefish.xpm");
+	str[2] = ft_strdup("./assets/fish.xpm");
+	i = 3;
+	while (++i < 7)
+	{
+	m->mlx_imgs[i] = mlx_xpm_file_to_image(m->mlx, str[i - 4],
+				&m->texture_data[i].width,
+				&m->texture_data[i].height);
+	m->im[i].img_data = mlx_get_data_addr(m->mlx_imgs[i],
+				&m->im[i].img_bp,
+				&m->im[i].img_sl,
+				&m->im[i].img_e);
+	m->texture_data[i].arr_color = create_color_arr(&m->im[i],
+				m->texture_data[i].height,
+				m->texture_data[i].width);
+	m->texture_data[i].img_h = m->mlx_imgs[i];
+	}
+	free_2d_array(str);
 }
 
 void	init_sprites(t_mlx_info *mlx_info)
 {
-	mlx_info->sprites->z_buff = ft_calloc(mlx_info->window_width, sizeof(double));
-	mlx_info->sprites->sprite_order = ft_calloc(mlx_info->sprites->sprite_count, sizeof(int));
-	mlx_info->sprites->sprite_distance = ft_calloc(mlx_info->sprites->sprite_count, sizeof(double));
+	t_sprite_data	*s;
+
+	s = mlx_info->sprites;
+	s->z_buff = ft_calloc(mlx_info->window_width, sizeof(double));
+	s->sprite_order = ft_calloc(s->sprite_count, sizeof(int));
+	s->sprite_distance = ft_calloc(s->sprite_count, sizeof(double));
 }
